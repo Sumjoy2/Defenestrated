6c08757489af8907dd7c39b97a354997dc86c6ee
Sumjoy2 <5011286mountjoy@gmail.com>
2023-03-20 13:42:36 -0700

    boss stuff totally

---
+++ Assets/Code/Boss.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    float invincibleTimer;
+
+    public int range;
+    public GameObject prot1;
+    public GameObject prot2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
