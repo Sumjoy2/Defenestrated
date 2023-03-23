c6dbac238ec0a8e6e8c073f75132cf8f49312b3b
Sumjoy2 <5011286mountjoy@gmail.com>
2023-03-23 13:44:32 -0700

    BossWork

--- Assets/Code/Boss.cs
+++ Assets/Code/Boss.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     float invincibleTimer;
+    float timerTimer = 5;
 
     public int range;
-    public GameObject prot1;
+    public GameObject protFire;
     public GameObject prot2;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerTimer <= 2.5f)
+        {
+            LaunchFireBall();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Boss Got Hit", other.otherCollider);
+    }
+
+    void LaunchFireBall()
+    {
+        GameObject fireball = Instantiate(protFire, transform.position, transform.rotation);
+        fireball.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector3(300, Random.Range(-range, range), 0));
     }
 }
---
+++ Assets/Code/bossProjectile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossProjectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("HitSomething");
+        Destroy(gameObject);
+    }
+}
