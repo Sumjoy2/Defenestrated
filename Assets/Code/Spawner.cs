664cadda58a1d16bce03cfe9c1a3bfe15ecbec52
Ranu <4041689@gmail.com>
2023-03-23 13:44:19 -0700

    still working on this so its gonna create trouble

--- Assets/Code/Spawner.cs
+++ Assets/Code/Spawner.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject cultWithKnifePrefab;
+    [SerializeField]
+    private GameObject cultWithGunPrefab;
+
+    private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemyWave(waveNumber);
+        Instantiate(cultWithKnifePrefab, GSP(), cultWithKnifePrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(cultWithKnifePrefab, GSP(), cultWithKnifePrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GSP(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    // generates random spawn position
+    private Vector3 GSP()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
     }
 }
