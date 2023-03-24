37aa77d572c7090c8eba86bc61370658791e13ef
Ranu <4041689@gmail.com>
2023-03-24 13:38:44 -0700

    teehee

--- Assets/Code/Gun.cs
+++ Assets/Code/Gun.cs
@@ -6,29 +6,21 @@ public class Gun : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
-    private Rigidbody2D rb;
-
     public float bulletForce = 10f;
 
-    // Update is called once per frame
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-
-    }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+            Fire();
         }          
     }
 
-    void Shoot()
+    void Fire()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
     }
 
 }
