82846555ef3f8fba4209e61204ced684ed49d868
Ranu <4041689@gmail.com>
2023-03-24 13:44:11 -0700

    mini things

--- Assets/Code/Bullet.cs
+++ Assets/Code/Bullet.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 5f;
-    
-    void Update()
-    {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    }
     
     void OnCollisionEnter2D(Collision2D collision)
     {        
