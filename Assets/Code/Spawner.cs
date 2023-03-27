d1870322341c0ced53eb4b34eab4d4d4ccb7f142
Ranu <4041689@gmail.com>
2023-03-27 12:07:44 -0700

    spawner

--- Assets/Code/Spawner.cs
+++ Assets/Code/Spawner.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    
+    public float spawnRate = 1.0f;
+    public float timeBetweenWaves = 5.0f;
+
+    public int enemyCount;
+    public GameObject enemy;
+    bool waveIsDone = true;
+
+    void Update()
+    {
+        if(waveIsDone == true)
+        {
+            StartCoroutine(waveSpawner());
+        }
+    }
+
+    IEnumerator waveSpawner()
+    {
+        waveIsDone = false;
+
+        for(int i = 0; i < enemyCount; i++)
+        {
+            GameObject enemyClone = Instantiate(enemy);
+            yield return new WaitForSeconds(spawnRate);
+        }
+
+        spawnRate -= 0.1f;
+        enemyCount += 5;
+
+        yield return new WaitForSeconds(timeBetweenWaves);
+
+        waveIsDone = true;
+    }
     
 }
