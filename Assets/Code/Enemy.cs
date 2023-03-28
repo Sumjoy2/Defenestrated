474814b21351f99c7435d159e65b4268508dfc0c
Ranu <4041689@gmail.com>
2023-03-28 13:42:16 -0700

    enemies follow, player can spin

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform player;
+    GameObject player;
     public float speed = 2f;
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private Vector3 movement;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
+
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
-        direction.Normalize();
-        movement = direction;
+        //Vector3 direction = player.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        //direction.Normalize();
+        //movement = direction;
+
+        moveCharacter();
     }
 
     void FixedUpdate()
     {
-        moveCharacter(movement);
+        //moveCharacter(movement);
     }
 
-    void moveCharacter(Vector2 direction)
+    void moveCharacter(/*Vector2 direction*/)
     {
-        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+        movement = (player.transform.position - transform.position).normalized;
+        rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
+        
+        //rb.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
     }
 }
