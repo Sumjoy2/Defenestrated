d52856bfacc150a7dd2e35b19cec62f398e848c2
Sumjoy2 <5011286mountjoy@gmail.com>
2023-05-04 12:57:09 -0700

    CameraWorks

---
+++ Assets/Code/CameraStuffs.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraStuffs : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vcam = GetComponent<CinemachineVirtualCamera>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        vcam.Follow = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
