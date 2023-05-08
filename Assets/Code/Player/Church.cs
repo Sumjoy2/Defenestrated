7eb6e3554764edd0fe403c2956d9a2bfbe7283ff
Ranu <4041689@gmail.com>
2023-05-08 13:43:03 -0700

    ;-;

---
+++ Assets/Code/Player/Church.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Church : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Bossfight");
+            player.position = targetPosition;
+        }
+    }
+}
